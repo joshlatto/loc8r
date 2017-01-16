bd53328d08c90885748806a03f425af187932709
Josh Latto <josh.latto@gmail.com>
2017-01-15 19:36:32 -0800

    Added some additional placeholder content

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,14 +1,14 @@
 /* GET 'home' page */
 module.exports.homeList = function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', content: 'Welcome to the home page! This is where a list of locations will eventually go. Thanks for visiting!' });
 };
 
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res, next) {
-  res.render('index', { title: 'Location info' });
+  res.render('index', { title: 'Location info', content: 'This is a placeholder for the information page of a specific location.' });
 };
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res, next) {
-  res.render('index', { title: 'Add review' });
+  res.render('index', { title: 'Add review', content: 'This is the placeholder for the page you\'ll use to add a review of a location' });
 };
