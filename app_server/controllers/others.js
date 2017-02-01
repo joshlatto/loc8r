e4a34799ac75227bafe8035a78d264f3d16b8fe4
joshlatto <josh.latto@gmail.com>
2017-02-01 11:09:26 -0800

    Move index.jade to generic-text.jade and update the /about controller accordingly.

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,5 @@
 
 /* GET About page */
 module.exports.about  = function(req, res, next) {
-  res.render('index', { title: 'About' , content: 'This is the place holder for the about page! Huzzah!.'});
+  res.render('generic-text', { title: 'About' , content: 'This is the place holder for the about page! Huzzah!.'});
 }; 
